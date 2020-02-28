5b58449ccddd52a0d2611d1cce645cf1f3ce5fe9
KEDRIA <aleksandrkedria@gmail.com>
2020-02-28 18:04:33 +0200

    kedria

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import './helpers/postDate';
 import scrollSmooth from './helpers/smooth-scroll.js';
 import example from './modules/example.js';
+import symptoms from './modules/symptoms.js';
 
 function main () {
   scrollSmooth();
   example();
+  symptoms();
 }
 
 main();
