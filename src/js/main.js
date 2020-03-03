aa2ba839582ea513f2488b8b3f1d2a5a976e23b6
O.M <a.majstrenko@everad.com>
2020-03-03 12:31:49 +0200

    add mob menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 import './helpers/postDate';
 import scrollSmooth from './helpers/smooth-scroll.js';
+import menu from './modules/menu.js';
 import symptoms from './modules/symptoms.js';
 
 function main () {
   scrollSmooth();
+  menu();
   symptoms();
 }
 
---
+++ src/js/modules/menu.js
@@ -0,0 +1,32 @@
+export default () => {
+    function showMenu() {
+      document.querySelector('.menu__overlay').style.display = 'block';
+      document.querySelector('.menu__wrapper').classList.add('active');
+      document.querySelector('html').style.overflowY = 'hidden';
+    }
+  
+    function closeMenu() {
+      document.querySelector('.menu__overlay').style.display = 'none';
+      document.querySelector('.menu__wrapper').classList.remove('active');
+      document.querySelector('html').style.overflowY = 'auto';
+    }
+  
+    document.querySelector('.menu__button').addEventListener('click', showMenu);
+    document.querySelector('.menu__close').addEventListener('click', closeMenu);
+  
+    window.addEventListener(`resize`, event => {
+      if (document.documentElement.clientWidth > 767) {
+        closeMenu();
+      }
+    }, false);
+  
+    let overlay = document.querySelector('.menu__overlay');
+  
+  
+    document.addEventListener('click', function(e){
+      if (e.target == overlay) {
+        closeMenu();
+      }
+    })
+  };
+  
