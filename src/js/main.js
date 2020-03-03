01c28dffe2b4c64e3cbbb35458d08bcf9d77c151
O.M <a.majstrenko@everad.com>
2020-03-03 19:30:04 +0200

    review

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,14 @@ import scrollSmooth from './helpers/smooth-scroll.js';
 import menu from './modules/menu.js';
 import symptoms from './modules/symptoms.js';
 import components from './modules/components.js';
+import reviews from './modules/reviews.js';
 
 function main() {
   scrollSmooth();
   menu();
   symptoms();
   components();
+  reviews();
 }
 
 main();
