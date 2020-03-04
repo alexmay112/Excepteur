a73ba2a8d08fa367a401641f05df4a5a1883bd68
O.M <a.majstrenko@everad.com>
2020-03-04 12:41:01 +0200

    reviews adaptive

--- src/js/modules/reviews.js
+++ src/js/modules/reviews.js
@@ -4,14 +4,23 @@ export default () => {
     slidesToShow: 3,
     slidesToScroll: 3,
     arrows: true,
-    dots: true,
-    prevArrow: '<div class="slider-circle slider-circle--left"><svg class="slider__arrow slider__arrow--left" xmlns="http://www.w3.org/2000/svg" width="11" height="20" viewBox="0 0 11 20"><g><g><path fill="#fff" d="M10.567 10.952L2.469 19.05a1.318 1.318 0 1 1-1.865-1.865L7.77 10.02.605 2.856A1.32 1.32 0 0 1 2.47.99l8.097 8.098a1.319 1.319 0 0 1 0 1.864z"/></g></g></svg></div>',
-    nextArrow: '<div class="slider-circle slider-circle--right"><svg class="slider__arrow slider__arrow--right" xmlns="http://www.w3.org/2000/svg" width="11" height="20" viewBox="0 0 11 20"><g><g><path fill="#fff" d="M10.567 10.952L2.469 19.05a1.318 1.318 0 1 1-1.865-1.865L7.77 10.02.605 2.856A1.32 1.32 0 0 1 2.47.99l8.097 8.098a1.319 1.319 0 0 1 0 1.864z"/></g></g></svg></div>',
-    responsive: [{
-        breakpoint: 768,
+    dots: false,
+    prevArrow: '<div class="slider-circle slider-circle--left"><svg class="slider__arrow slider__arrow--left" xmlns="http://www.w3.org/2000/svg" width="11" height="20" viewBox="0 0 11 20"><g><g><path d="M10.567 10.952L2.469 19.05a1.318 1.318 0 1 1-1.865-1.865L7.77 10.02.605 2.856A1.32 1.32 0 0 1 2.47.99l8.097 8.098a1.319 1.319 0 0 1 0 1.864z"/></g></g></svg></div>',
+    nextArrow: '<div class="slider-circle slider-circle--right"><svg class="slider__arrow slider__arrow--right" xmlns="http://www.w3.org/2000/svg" width="11" height="20" viewBox="0 0 11 20"><g><g><path d="M10.567 10.952L2.469 19.05a1.318 1.318 0 1 1-1.865-1.865L7.77 10.02.605 2.856A1.32 1.32 0 0 1 2.47.99l8.097 8.098a1.319 1.319 0 0 1 0 1.864z"/></g></g></svg></div>',
+    responsive: [
+      {
+        breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2
+        }
+      },
+     
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       },
       {
@@ -23,4 +32,13 @@ export default () => {
       }
     ]
   });
+
+  $('.review__slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    $('.review__item').removeClass('active');
+  });
+
+  $('.review__btn').on('click', function (e) {
+    $(this).parents('.review__item').toggleClass('active');
+  })
+
 };
